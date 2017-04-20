9eff47850e18f971f1599f568eabc9c11db28641
shettyprabodh <shettyprabodh@gmail.com>
2017-04-20 22:04:26 +0530

    Movie recommendation version 1.0

--- app.py
+++ app.py
@@ -9,9 +9,17 @@ from chatterbot import ChatBot
 from chatterbot.trainers import ListTrainer
 from random import randint
 from ourDataset import conversations
+import random
+import json
+#import requests_cache
+import requests
 
 # Declares flask app
 app = Flask(__name__)
+
+proxies = {'http':'http://202.141.80.24:3128', 'https':'https://202.141.80.24:3128'}
+auth = requests.auth.HTTPProxyAuth('username', 'passwd')
+
 
 # For Twilio
 client = TwilioRestClient(settings['sid'], settings['auth'])
@@ -77,6 +85,71 @@ def chat():
 @app.route('/settings')
 def settings():
 	return render_template('settings.html')
+
+
+
+@app.route('/com_mov', methods=['POST','GET'])
+def recommend_comedy_mov():
+	file = open("comedy_movies.txt",'r')
+	com_mov_list = []
+	for line in file.readlines():
+		com_mov_list.append(line)
+	print "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+	#return random.choice(com_mov_list)
+	#with requests_cache.enabled('movie_cache', backend='sqlite', expire_after=86400):
+
+	temp = 'http://www.omdbapi.com/?t=' + str(random.choice(com_mov_list)) + '&plot=full&r=json'
+	print temp
+	#r = requests.get(temp)
+
+	r = requests.get(temp, proxies = proxies, auth = auth)
+
+	data = r.json()
+	print data
+	print "==========================================================="
+	return str('<img src=\'') + str(data['Poster']) + str('\'/> <p>') + str(data['Title']) + str('</p>')
+
+@app.route('/mot_mov', methods=['POST','GET'])
+def recommend_motivational_mov():
+	file = open("motivational_movies.txt",'r')
+	mot_mov_list = []
+	for line in file.readlines():
+		mot_mov_list.append(line)
+	print "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+	#return random.choice(mot_mov_list)
+	#with requests_cache.enabled('movie_cache', backend='sqlite', expire_after=86400):
+
+	temp = 'http://www.omdbapi.com/?t=' + str(random.choice(mot_mov_list)) + '&plot=full&r=json'
+	print temp
+	#r = requests.get(temp)
+
+	r = requests.get(temp, proxies = proxies, auth = auth)
+
+	data = r.json()
+	print data
+	print "==========================================================="
+	return str('<img src=\'') + str(data['Poster']) + str('\'/> <p>') + str(data['Title']) + str('</p>')
+
+@app.route('/feel_good_mov', methods=['POST','GET'])
+def recommend_feel_good_mov():
+	file = open("feel_good_movies.txt",'r')
+	feel_good_mov_list = []
+	for line in file.readlines():
+		feel_good_mov_list.append(line)
+	print "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+	#return random.choice(feel_good_mov_list)
+	#with requests_cache.enabled('movie_cache', backend='sqlite', expire_after=86400):
+
+	temp = 'http://www.omdbapi.com/?t=' + str(random.choice(feel_good_mov_list)) + '&plot=full&r=json'
+	print temp
+	#r = requests.get(temp)
+
+	r = requests.get(temp, proxies = proxies, auth = auth)
+
+	data = r.json()
+	print data
+	print "==========================================================="
+	return str('<img src=\'') + str(data['Poster']) + str('\'/> <p>') + str(data['Title']) + str('</p>')
 
 # Handle receiving text messages
 @app.route('/api/receive', methods=['POST'])
@@ -158,4 +231,4 @@ def process_message():
 
 
 if __name__ == '__main__':
-  app.run()
+  app.run(debug = True)
